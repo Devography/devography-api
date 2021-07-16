7dc20a148bbc226d334312b2d81d0a69ef80f49c
income3000 <income3000@gmail.com>
2021-07-16 04:27:42 -0400

    fixed what I broke

--- controllers/comments.js
+++ controllers/comments.js
@@ -6,37 +6,37 @@ const Language = require('../models/languageSchema')
 
 
 router.get('/', async (req, res) => {
-    try {
-        const languages = await Language.find();
-        res.json(languages);
-    } catch (err) {
-        console.error(err);
-    }
+	try {
+		const languages = await Language.find();
+		res.json(languages);
+	} catch (err) {
+		console.error(err);
+	}
 });
 router.get('/', async (req, res) => {
-    try {
-        const languages = await Language.findById(req.params.id);
-        res.json(languages);
-    } catch (err) {
-        console.error(err);
-    }
+	try {
+		const languages = await Language.findById(req.params.id);
+		res.json(languages);
+	} catch (err) {
+		console.error(err);
+	}
 });
 
 
 router.put('/:id', async (req, res) => {
-    try {
-        const language = await Language.findByIdAndUpdate(req.params.id);
+	try {
+		const language = await Language.findByIdAndUpdate(req.params.id);
         res.json(language);
-    } catch (err) {
-        console.error(err);
-    }
+	} catch (err) {
+		console.error(err);
+	}
 });
 
 
 router.post('/:id', async (req,res) =>{
-    try{
+	try{
     console.log(req.body)
-        
+		
    const language = await Language.findById(req.params.id)
 
    language.comments.push(req.body);
@@ -60,14 +60,14 @@ router.delete('/:id', async(req,res)=>{
         console.log(err)
     }
 })
-router.delete('/:id', (req, res, next) => {
-    const id = req.params.id;
-    Language.findOne({ 'comments._id': id })
-        .then((language) => {
-            language.comments.id(id).remove();
-            return language.save();
-        })
-        .then(() => res.sendStatus(204))
-        .catch(next);
+router.delete('/comments/:id', (req, res, next) => {
+	const id = req.params.id;
+	Language.findOne({ 'comment._id': id })
+		.then((language) => {
+			language.reviews.id(id).remove();
+			return language.save();
+		})
+		.then(() => res.sendStatus(204))
+		.catch(next);
 });
 module.exports = router
