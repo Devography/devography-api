5c6b43a3fed6b33c83f565eca72aead68211e90b
income3000 <income3000@gmail.com>
2021-07-16 03:19:24 -0400

    redone post routes

--- controllers/comments.js
+++ controllers/comments.js
@@ -6,38 +6,48 @@ const Language = require('../models/languageSchema')
 
 
 router.get('/', async (req, res) => {
-	try {
-		const languages = await Language.find();
-		res.json(languages);
-	} catch (err) {
-		console.error(err);
-	}
+    try {
+        const languages = await Language.find();
+        res.json(languages);
+    } catch (err) {
+        console.error(err);
+    }
 });
 router.get('/', async (req, res) => {
-	try {
-		const languages = await Language.findById(req.params.id);
-		res.json(languages);
-	} catch (err) {
-		console.error(err);
-	}
+    try {
+        const languages = await Language.findById(req.params.id);
+        res.json(languages);
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 
 router.put('/:id', async (req, res) => {
-	try {
-		const language = await Language.findByIdAndUpdate(req.params.id);
+    try {
+        const language = await Language.findByIdAndUpdate(req.params.id);
         res.json(language);
-	} catch (err) {
-		console.error(err);
-	}
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 
-router.post('/', async (req,res) =>{
+router.post('/:id', async (req,res) =>{
+    try{
     console.log(req.body)
-   const newLanguage = await Language.create(req.body)
-   res.json(newLanguage)
+        
+   const language = await Language.findById(req.params.id)
+
+   language.comments.push(req.body);
+   const updatedLanguage = await language.save();
+   res.status(201).json(updatedLanguage);
+}catch(err){
+   console.log(err)
+}
+
 })
+
 
 
 
@@ -50,5 +60,14 @@ router.delete('/:id', async(req,res)=>{
         console.log(err)
     }
 })
-
+router.delete('/:id', (req, res, next) => {
+    const id = req.params.id;
+    Language.findOne({ 'comments._id': id })
+        .then((language) => {
+            language.comments.id(id).remove();
+            return language.save();
+        })
+        .then(() => res.sendStatus(204))
+        .catch(next);
+});
 module.exports = router
